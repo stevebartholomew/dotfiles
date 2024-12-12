74d8c6fdcdc29ccc3702c4b661b054eb86c32c2c
Stephen Bartholomew <stephen.b@meetcleo.com>
2024-12-12 19:38:32 +0000

    keymaps and plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,6 +3,7 @@ vim.opt.background = "dark" -- Set background mode
 vim.cmd("colorscheme twilight256")
 vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
 
+vim.opt.wrap = false
 vim.opt.number = true -- Show line numbers
 
 vim.g.mapleader = "," -- Set the leader key
@@ -19,8 +20,17 @@ end
 vim.cmd([[
 call plug#begin()
 Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-tree/nvim-web-devicons'
+Plug 'nvim-neo-tree/neo-tree.nvim'
+Plug 'MunifTanjim/nui.nvim'
 Plug 'nvim-telescope/telescope.nvim', { 'tag': '0.1.8' }
 Plug 'neovim/nvim-lspconfig'
+
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
+Plug 'hrsh7th/nvim-cmp'
 call plug#end()
 ]])
 
@@ -31,11 +41,23 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
+-- pane navigation
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
 
--- ruby-lsp
-require'lspconfig'.ruby_lsp.setup{}
+vim.api.nvim_set_keymap('n', '<C-a>', ':tabprevious<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-e>', ':tabnew<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-q>', ':tabclose<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-d>', ':tabnext<CR>', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>n', ':Neotree reveal<CR>', {})
+vim.keymap.set('n', '<C-f>', ':Neotree reveal<CR>', {})
 
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = "Go to implementation" })
+vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = "Go to references" })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.rb",
@@ -43,3 +65,59 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     vim.lsp.buf.format()
   end,
 })
+
+-- nvim-cmp
+
+-- Set up nvim-cmp.
+local cmp = require'cmp'
+
+cmp.setup({
+snippet = {
+-- REQUIRED - you must specify a snippet engine
+expand = function(args)
+vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+end,
+},
+window = {
+-- completion = cmp.config.window.bordered(),
+-- documentation = cmp.config.window.bordered(),
+},
+mapping = cmp.mapping.preset.insert({
+['<C-b>'] = cmp.mapping.scroll_docs(-4),
+['<C-f>'] = cmp.mapping.scroll_docs(4),
+['<C-Space>'] = cmp.mapping.complete(),
+['<C-e>'] = cmp.mapping.abort(),
+['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+}),
+sources = cmp.config.sources({
+{ name = 'nvim_lsp' },
+}, {
+{ name = 'buffer' },
+})
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+mapping = cmp.mapping.preset.cmdline(),
+sources = {
+{ name = 'buffer' }
+}
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+mapping = cmp.mapping.preset.cmdline(),
+sources = cmp.config.sources({
+{ name = 'path' }
+}, {
+{ name = 'cmdline' }
+}),
+matching = { disallow_symbol_nonprefix_matching = false }
+})
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+require('lspconfig')['ruby_lsp'].setup {
+	capabilities = capabilities
+}
