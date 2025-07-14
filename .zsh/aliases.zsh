1657ef1521302cf5738ae749a32132f8e78ce79b
Stephen Bartholomew <stephen.b@meetcleo.com>
2025-07-14 09:44:21 +0100

    push create on origin alais

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -4,3 +4,4 @@ alias rs='RAILS_ENV=test ./bin/rspec'
 alias be='bundle exec'
 
 alias gb="git for-each-ref --sort=-committerdate --format='%(committerdate:iso8601) %(refname:short)' refs/heads/ | head"
+alias gpo="git push --set-upstream origin $(git branch --show-current)"
