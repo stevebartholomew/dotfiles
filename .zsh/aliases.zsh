471ee93ca3a5491a8f1fba751c81f48805599ae7
Stephen Bartholomew <stephen.b@meetcleo.com>
2025-07-14 09:28:30 +0100

    alias for listing git branches by last commit

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -2,3 +2,5 @@ alias r='./bin/rails'
 alias g='git'
 alias rs='RAILS_ENV=test ./bin/rspec'
 alias be='bundle exec'
+
+alias gb="git for-each-ref --sort=-committerdate --format='%(committerdate:iso8601) %(refname:short)' refs/heads/ | head"
